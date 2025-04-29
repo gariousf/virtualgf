2a145ca012d289ee0f782ef0d02e7cc0d0716ab1
Olanrewaju Oladunjoye <olanrewaju.oladunjoye@web-veistamo.fi>
2025-04-29 06:12:58 +0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true // Allow access from network
+  },
+  preview: {
+    host: true, // Allow access from network
+    port: 4173, // Default preview port, ensure it matches Dockerfile EXPOSE
+    allowedHosts: [
+      'virtualgf-tpr2.onrender.com',
+      // Add any other allowed hosts here, e.g., localhost for local testing
+      'localhost'
+    ],
+  },
 })
